63581994416c8c430290699a3dd845b784d042cb
Pkolkiew <Pkolkiew@gmail.com>
2019-11-14 12:02:23 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# config-server 
+
+Config server hosting configuration files to my spring cloud apps.
+Repository which serves as a configuration source can be found here: 
+<u>https://github.com/pkolkiew/config-server-data-v1</u>
---
+++ src/test/java/pl/pk/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pk.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
